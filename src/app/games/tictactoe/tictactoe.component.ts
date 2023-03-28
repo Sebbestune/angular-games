881ebffd9a3064044250003b3356f54cc9460ac8
sebastian.tb.lindgren <sebastian.tb.lindgren@gmail.com>
2023-03-29 00:47:57 +0300

    Base functionality

--- src/app/games/tictactoe/tictactoe.component.ts
+++ src/app/games/tictactoe/tictactoe.component.ts
@@ -13,16 +13,31 @@ class GridItem implements IGridItem {
     this.clicked = false;
     this.value = "_";
   }
+
+  set(turn : boolean){
+    this.clicked = true;
+    turn ? this.value = "X" :  this.value = "O";
+  }
+
+  ignore(){
+    console.log("Don't click a set board-tile.")
+  }
 }
 
 class GameBoard {
   gridItems: GridItem[];
+  turn = true;
 
   constructor(){
     this.gridItems = [];
     for(let i = 0; i < 9; i++){
       this.gridItems.push(new GridItem);
     }
+  }
+
+  changeTurn(){
+    console.log(this.turn)
+    return this.turn ? this.turn = false : this.turn = true;
   }
 }
 
